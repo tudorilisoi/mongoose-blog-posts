434d2d4864a206b08acd32ded639651f51855435
Taradacktl <35745484+Taradacktl@users.noreply.github.com>
2018-04-24 15:43:09 -0700

    Update models.js

--- models.js
+++ models.js
@@ -2,22 +2,12 @@ const mongoose = require('mongoose');
 
 // this is our schema to represent a restaurant
 const blogPostSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  borough: {type: String, required: true},
-  cuisine: {type: String, required: true},
-  address: {
-    building: String,
-    // coord will be an array of string values
-    coord: [String],
-    street: String,
-    zipcode: String
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: String,
+    lastName: String
   },
-  // grades will be an array of objects
-  grades: [{
-    date: Date,
-    grade: String,
-    score: Number
-  }]
 });
 
 // *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
@@ -25,8 +15,8 @@ const blogPostSchema = mongoose.Schema({
 // properties that are stored in the database. Here we use it
 // to generate a human readable string based on the address object
 // we're storing in Mongo.
-blogPostSchema.virtual('addressString').get(function() {
-  return `${this.address.building} ${this.address.street}`.trim()});
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
 
 // this virtual grabs the most recent grade for a BlogPost.
 blogPostSchema.virtual('grade').get(function() {
@@ -38,14 +28,11 @@ blogPostSchema.virtual('grade').get(function() {
 // of the model. This method will be used to return an object that only
 // exposes *some* of the fields we want from the underlying data
 blogPostSchema.methods.serialize = function() {
-
   return {
     id: this._id,
-    name: this.name,
-    cuisine: this.cuisine,
-    borough: this.borough,
-    grade: this.grade,
-    address: this.addressString
+    title: this.title,
+    content: this.content,
+    author: this.authorName
   };
 }
 
