c4ebb38aecba800b5c35acdd4a355bde8dbea354
Taradacktl <35745484+Taradacktl@users.noreply.github.com>
2018-04-24 15:44:44 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
 exports.PORT = process.env.PORT || 8080;
