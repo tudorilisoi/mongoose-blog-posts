7c6c85a1a2f16379ac1be70db4e0174c749cc889
Tara Ragsdale <Taradacktl@aol.com>
2018-04-26 08:45:11 -0700

    update config

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/mongoose-blog-app';
 exports.PORT = process.env.PORT || 8080;
