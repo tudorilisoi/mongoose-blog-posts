f12a472df97ceb6c674304d9269996433ad9e10f
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-04-29 11:34:26 +0300

    logging DATABASE_URL

--- server.js
+++ server.js
@@ -159,6 +159,7 @@ function closeServer() {
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
+  console.log('DATABASE_URL is: ', DATABASE_URL)
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
